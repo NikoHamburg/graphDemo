5c77d7e0bd262d3e71357addbfa172bcb8d4c6b0
NikoHamburg <nikotrettin@googlemail.com>
2022-02-21 18:20:03 +0100

    cleanup 2

--- 2-ApolloExpress/server.js
+++ 2-ApolloExpress/server.js
@@ -4,9 +4,11 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-co
 
 const port = 4040;
 
-const typeDefs = `type Query {
+const typeDefs = `
+  type Query {
     info: String!
-}`;
+  }
+  `;
 
 const resolvers = {
   Query: {
